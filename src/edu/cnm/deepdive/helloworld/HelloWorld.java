a22df6b4eb2f0de29e90231b2973d20f20496d3d
toddnordquist <toddnordquist144@gmail.com>
2018-11-10 08:51:45 -0700

    New project files, with file implementing "Hello World!" program.

---
+++ src/edu/cnm/deepdive/helloworld/HelloWorld.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package edu.cnm.deepdive.helloworld;
+
+/**
+ * @author toddnordquist
+ *
+ * This class displays the "Hello World!" message
+ */
+public class HelloWorld {
+
+  /**
+   * This method prints "Hello World!" to standard output.
+   * 
+   * @param args Command-line parameters (ignored).
+   */
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+
+}
